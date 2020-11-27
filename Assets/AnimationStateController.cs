abe47b1b0b4cb2186915a3e75b270a70810e7477
tommercoder <48603935+tommercoder@users.noreply.github.com>
2020-11-27 22:20:01 +0200

    animation setting up first

--- Assets/AnimationStateController.cs
+++ Assets/AnimationStateController.cs
@@ -5,29 +5,36 @@ using UnityEngine;
 public class AnimationStateController : MonoBehaviour
 {
     Animator animator;
-    int isRunningHash,isWalkingBackHash,isDrawedSwordHash,isIdleSwordHash,isRunningSwordHash,isRunningBackSwordHash;
+    public float timeRemaining = 1;
+    public bool timerIsRunning = false;
+    int isRunningHash,isWalkingBackHash,isDrawedSwordHash,isIdleSwordHash,isRunningSwordHash,isRunningBackSwordHash, isSneathedHash;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        timerIsRunning = true;
         isRunningHash = Animator.StringToHash("isRunning");
         isWalkingBackHash = Animator.StringToHash("isWalkingBack");
         isDrawedSwordHash = Animator.StringToHash("isDrawedSword");
         isIdleSwordHash = Animator.StringToHash("isIdleSword");
         isRunningSwordHash = Animator.StringToHash("isRunningSword");
         isRunningBackSwordHash = Animator.StringToHash("isRunningSwordBack");
+        isSneathedHash = Animator.StringToHash("isSneathed");
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
         bool isRunning = animator.GetBool(isRunningHash);
         bool isWalkingBack = animator.GetBool(isWalkingBackHash);
         bool isIdleSword = animator.GetBool(isIdleSwordHash);
         bool isDrawedSword = animator.GetBool(isDrawedSwordHash);
         bool isRunningSword = animator.GetBool(isRunningSwordHash);
         bool isRunningSwordBack = animator.GetBool(isRunningBackSwordHash);
-        
+        bool isSneathedSword = animator.GetBool(isSneathedHash);
+
         bool runningPressed = Input.GetKey("w");
         bool walkingBackPressed = Input.GetKey("s");
         bool takeSwordPressed = Input.GetKey(KeyCode.Mouse0);
@@ -54,6 +61,9 @@ public class AnimationStateController : MonoBehaviour
         {
             animator.SetBool(isDrawedSwordHash, true);
             animator.SetBool(isRunningSwordHash, true);
+            animator.SetBool(isSneathedHash, false);
+            timeRemaining = 5;
+            timerIsRunning = true;
 
         }
         //if doesnt run and already have sword go to idle with sword
@@ -73,17 +83,70 @@ public class AnimationStateController : MonoBehaviour
         {
             animator.SetBool(isDrawedSwordHash, true);
             animator.SetBool(isIdleSwordHash, true);
+            animator.SetBool(isSneathedHash, false);
+            timeRemaining =5;
+            timerIsRunning = true;
         }
         //if walking back and pressed sword draw then run back with sword
-        if(isWalkingBack && takeSwordPressed)
+        if(walkingBackPressed && !isDrawedSword && takeSwordPressed)
         {
+            
             animator.SetBool(isDrawedSwordHash, true);
             animator.SetBool(isRunningBackSwordHash, true);
         }
+        //if we are not already running back then go to idle sword
+        if(!walkingBackPressed && isDrawedSword && !takeSwordPressed)
+        {
+            animator.SetBool(isRunningBackSwordHash, false);
+            animator.SetBool(isIdleSwordHash, true);
+        }
         //if idle sword and pressed back run back with sword
-        if(!isRunning && isDrawedSword && !takeSwordPressed && !isWalkingBack && walkingBackPressed)
+        if(isDrawedSword && walkingBackPressed && !takeSwordPressed)
         {
             animator.SetBool(isRunningBackSwordHash, true);
+        }
+        //when we are running with sword and there are no enemies around
+        if (isDrawedSword)
+        {
+            
+            if (timerIsRunning)
+            {
+               
+                if (timeRemaining > 0 && isDrawedSword)
+                {
+                    timeRemaining -= Time.deltaTime;
+                }
+                else
+                {
+                    if (isDrawedSword && isRunningSword)//add enemies detection and check if there are no enemies when we are going to sneathe
+                    {
+                        animator.SetBool("isSneathed", true);
+                        animator.SetBool(isRunningHash, true);
+                        animator.SetBool("isDrawedSword", false);
+                        animator.SetBool(isRunningSwordHash, false);
+                      
+
+                    }
+                    if (isDrawedSword && isIdleSword)//add enemies detection and check if there are no enemies when we are going to sneathe
+                    {
+                        animator.SetBool(isSneathedHash, true);
+                        animator.SetBool(isRunningHash, false);
+                        animator.SetBool(isDrawedSwordHash, false);
+                       
+
+                        Debug.Log("Sneathed" + isSneathedSword);
+                        Debug.Log("Drawed" + isDrawedSword);
+                    }
+                    
+                    if(isSneathedSword)
+                    {
+                        animator.SetBool(isSneathedHash, false);
+                    }
+                    timeRemaining = 0;
+                    timerIsRunning = false;
+
+                }
+            }
         }
 
     }
