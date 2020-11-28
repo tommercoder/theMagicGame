e81f746619dfc712a72c8d7ccef729549fbdd1db
tommercoder <48603935+tommercoder@users.noreply.github.com>
2020-11-29 00:48:36 +0200

    animation setting up

---
+++ Assets/Scripts/playerSword.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerSword : MonoBehaviour
+{
+    public  Transform sword, swordEquiped, swordUnequiped;
+    public bool isEquipedSword = false;
+    private void Update()
+    {
+        if (isEquipedSword)
+        {
+            sword.position = swordEquiped.position;
+            sword.rotation = swordEquiped.rotation;
+        }
+        else 
+        {
+            sword.position = swordUnequiped.position;
+            sword.rotation = swordUnequiped.rotation;
+        }
+    }
+    public void swordEquipMethod()
+    {
+        isEquipedSword = true;
+    }
+    public void swordUnequipMethod()
+    {
+        isEquipedSword = false;
+    }
+
+}
