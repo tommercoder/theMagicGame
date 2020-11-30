6134c19aa1f825967cfb74d266e84fe4fe26a52c
tommercoder <48603935+tommercoder@users.noreply.github.com>
2020-11-30 12:05:51 +0200

    first weapon swithing

--- Assets/Scripts/playerSword.cs
+++ Assets/Scripts/playerSword.cs
@@ -5,9 +5,45 @@ using UnityEngine;
 public class playerSword : MonoBehaviour
 {
     public  Transform sword, swordEquiped, swordUnequiped;
+    
+    public GameObject pearlSword, modernSword, darkSword;
     public bool isEquipedSword = false;
+    
+    private void Start()
+    {
+        modernSword = GameObject.Find("character/mixamorig:Hips/mixamorig:Spine/mixamorig:Spine1/ModernSword");
+        modernSword.SetActive(false);
+        
+        pearlSword = GameObject.Find("character/mixamorig:Hips/mixamorig:Spine/mixamorig:Spine1/PearlSword");
+        pearlSword.SetActive(false);
+        
+        darkSword = GameObject.Find("character/mixamorig:Hips/mixamorig:Spine/mixamorig:Spine1/DarkSword");
+        darkSword.SetActive(true);
+        
+    }
     private void Update()
     {
+        if (Input.GetKey("1"))
+        {
+            darkSword.SetActive(true);
+            pearlSword.SetActive(false);
+            modernSword.SetActive(false);
+            sword = darkSword   .transform;
+        }
+        if (Input.GetKey("2"))
+        {
+            darkSword.SetActive(false);
+            pearlSword.SetActive(false);
+            modernSword.SetActive(true);
+            sword = modernSword.transform;
+        }
+        if(Input.GetKey("3"))
+        {
+            darkSword.SetActive(false);
+            modernSword.SetActive(false);
+            pearlSword.SetActive(true);
+            sword = pearlSword.transform;
+        }
         if (isEquipedSword)
         {
             sword.position = swordEquiped.position;
@@ -27,5 +63,5 @@ public class playerSword : MonoBehaviour
     {
         isEquipedSword = false;
     }
-
+    
 }
