abf8b15fb8b07d232f40a07dfc993f13a579c52e
tommercoder <48603935+tommercoder@users.noreply.github.com>
2021-02-02 21:56:04 +0200

    trees,light settings,light probe,new custom shaders and textures

---
+++ Assets/Scripts/mainElementControlling/targetFrame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetFrame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 300;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
