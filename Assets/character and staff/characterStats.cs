22cdc92ec2bb3914bfffb35904c36de04500a9a1
tommercoder <48603935+tommercoder@users.noreply.github.com>
2021-03-06 19:08:12 +0200

    [TS] killing one leg procedural with pushing them backwards and also killing with fireball(added leveling class)

---
+++ Assets/character and staff/characterStats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterStats : MonoBehaviour
+{
+    #region 
+    public static characterStats instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+       #endregion
+    public int damageFromFireball;
+    public float timeOfSwordAbility;
+    public int lvl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lvl = 1;
+        damageFromFireball = lvl * 7;
+        timeOfSwordAbility = lvl * 1.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/effects/triggerFireBallScript.cs
+++ Assets/effects/triggerFireBallScript.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class triggerFireBallScript : MonoBehaviour
 {
+    public int damage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        damage = 10;
     }
 
     // Update is called once per frame
@@ -17,11 +18,15 @@ public class triggerFireBallScript : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Enemy"))
+        if(other.CompareTag("ENEMY"))
         {
             fireballAbility.instance.triggered = true;
 
             //make other.getComponent<HP>()-=damage;
+            other.gameObject.GetComponent<ProceduralStats>().currentHealth -= characterStats.instance.damageFromFireball;//this.gameObject.GetComponent<weaponInteract>().item.swordDamage;
+
+
+            other.gameObject.transform.DOMove(other.gameObject.transform.position + (-other.gameObject.transform.forward * 2), 0.2f);
         }
     }
 }
