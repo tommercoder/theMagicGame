b2205c78a84ecefda1cdca4de4a2a21a3b874ce6
tommercoder <48603935+tommercoder@users.noreply.github.com>
2021-04-04 22:52:06 +0300

    zone lake ended,restoring health temple done

--- Assets/Scripts/mainElementControlling/targetFrame.cs
+++ Assets/Scripts/mainElementControlling/targetFrame.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class targetFrame : MonoBehaviour
 {
+   
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 400;
+        Application.targetFrameRate = 200;
+        
     }
 
     // Update is called once per frame
